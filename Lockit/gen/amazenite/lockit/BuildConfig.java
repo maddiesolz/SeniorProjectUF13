1635404edfeaa0babcee32207626d469ae47435e
Marysol Ortiz <marysol@ufl.edu>
2013-01-24 14:53:03 -0500

    Project Created

---
+++ Lockit/gen/amazenite/lockit/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package amazenite.lockit;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Lockit/src/amazenite/lockit/Lockit.java
@@ -0,0 +1,141 @@
+package amazenite.lockit;
+
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class Lockit extends FragmentActivity implements ActionBar.TabListener {
+
+	/**
+	 * The serialization (saved instance state) Bundle key representing the
+	 * current tab position.
+	 */
+	private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lockit);
+
+		// Set up the action bar to show tabs.
+		final ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		// For each of the sections in the app, add a tab to the action bar.
+		actionBar.addTab(actionBar.newTab().setText(R.string.title_section1)
+				.setTabListener(this));
+		actionBar.addTab(actionBar.newTab().setText(R.string.title_section2)
+				.setTabListener(this));
+		actionBar.addTab(actionBar.newTab().setText(R.string.title_section3)
+				.setTabListener(this));
+	}
+
+	@Override
+	public void onRestoreInstanceState(Bundle savedInstanceState) {
+		// Restore the previously serialized current tab position.
+		if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
+			getActionBar().setSelectedNavigationItem(
+					savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
+		}
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		// Serialize the current tab position.
+		outState.putInt(STATE_SELECTED_NAVIGATION_ITEM, getActionBar()
+				.getSelectedNavigationIndex());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_lockit, menu);
+		return true;
+	}
+
+	@Override
+	public void onTabSelected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+		// When the given tab is selected, show the tab contents in the
+		// container view.
+		Fragment fragment = new SectionFragment();
+		Bundle args = new Bundle();
+		args.putInt(SectionFragment.ARG_SECTION_NUMBER,
+				tab.getPosition() + 1);
+		fragment.setArguments(args);
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.container, fragment).commit();
+		
+	}
+
+	@Override
+	public void onTabUnselected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+	}
+	
+
+	public void sendMessage() {
+	    Intent intent = new Intent(this, SetPic.class);
+	    startActivity(intent);
+	}
+
+	@Override
+	public void onTabReselected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+	}
+
+	/**
+	 * A dummy fragment representing a section of the app, but that simply
+	 * displays dummy text.
+	 */
+	public static class SectionFragment extends Fragment {
+		/**
+		 * The fragment argument representing the section number for this
+		 * fragment.
+		 */
+		public static final String ARG_SECTION_NUMBER = "section_number";
+
+		public SectionFragment() {
+		}
+		
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			// Create a new TextView and set its text to the fragment's section
+			// number argument value.
+			TextView textView = new TextView(getActivity());
+			textView.setGravity(Gravity.CENTER);
+			if(getArguments().getInt(ARG_SECTION_NUMBER) == 1)
+			{
+			return inflater.inflate(R.layout.sample, container, false);
+			}
+			if(getArguments().getInt(ARG_SECTION_NUMBER) == 2)
+			{
+				textView.setText("Voice");
+			}
+			if(getArguments().getInt(ARG_SECTION_NUMBER) == 3)
+			{
+				textView.setText("Settings");
+			}
+			
+			return textView;
+		}
+	}
+	
+	
+
+}
