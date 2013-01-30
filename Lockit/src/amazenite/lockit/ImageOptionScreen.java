864406ab284d3bab7f73ad7623bf9a364c54f3c7
Marysol Ortiz <marysol@ufl.edu>
2013-01-29 20:52:53 -0500

    Navigation Updated

--- Lockit/src/amazenite/lockit/ImageOptionScreen.java
+++ Lockit/src/amazenite/lockit/ImageOptionScreen.java
@@ -6,12 +6,20 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class ImageOptionScreen extends Activity {
 	
+	/** Called when the user clicks the get image button */
+	public void viewImages(View view) {
+	    // Do something in response to button
+		Intent intent = new Intent(this, OpenImages.class);
+		startActivity(intent);
+		
+	}
 
 
 	@Override
@@ -44,6 +52,7 @@ public class ImageOptionScreen extends Activity {
         }
         }
         
+       
 		
 	}
 
@@ -58,6 +67,7 @@ public class ImageOptionScreen extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
+			finish();
 			// This ID represents the Home or Up button. In the case of this
 			// activity, the Up button is shown. Use NavUtils to allow users
 			// to navigate up one level in the application structure. For
@@ -65,10 +75,10 @@ public class ImageOptionScreen extends Activity {
 			//
 			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
 			//
-			NavUtils.navigateUpFromSameTask(this);
+			//NavUtils.navigateUpFromSameTask(this);
 			return true;
 		}
-		return super.onOptionsItemSelected(item);
+		return false;//super.onOptionsItemSelected(item);
 	}
 
 }
