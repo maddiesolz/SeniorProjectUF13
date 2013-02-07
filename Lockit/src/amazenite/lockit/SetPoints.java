015d1b3cfaf282a3072460709a8caac56afa8d6a
Marysol Ortiz <marysol@ufl.edu>
2013-02-07 13:28:06 -0500

    Git Ignore, Capture Coordinates, Draw Debug

--- Lockit/src/amazenite/lockit/SetPoints.java
+++ Lockit/src/amazenite/lockit/SetPoints.java
@@ -4,13 +4,29 @@ import java.io.File;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.view.View; 
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.support.v4.app.NavUtils;
+import android.support.v4.view.GestureDetectorCompat;
 
-public class SetPoints extends Activity {
+
+public class SetPoints extends Activity { 
+	private int x = 0;
+ 	private int y = 0;
+ 
+ private GestureDetectorCompat mDetector; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +35,9 @@ public class SetPoints extends Activity {
 		// Show the Up button in the action bar.
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		
+
 		
-		
+		//Set As Background Image
 	    ImageView img = (ImageView) findViewById(R.id.setPointsPic);
 	    if(img != null)
 	    {
@@ -28,17 +45,28 @@ public class SetPoints extends Activity {
 		    String internalPath = "data/data/files/lockimg";
 		    if (file.exists()) {
 		    	 internalPath = file.getAbsolutePath();
+		    }
 		        	Drawable d = Drawable.createFromPath(internalPath);
-		         if(d!=null)
-		         {
+		        if(d!=null)
+		        {
 		        	 img.setImageDrawable(d);
 		        	 img.invalidate();
 		         }
 		    }
-	    }
-		
-		
+		   
+	    
+	    mDetector = new GestureDetectorCompat(this, new MyGestureListener());
+
 	}
+	
+
+	
+	   @Override 
+	    public boolean onTouchEvent(MotionEvent event){ 
+	        this.mDetector.onTouchEvent(event);
+	        
+	        return super.onTouchEvent(event);
+	    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,6 +74,7 @@ public class SetPoints extends Activity {
 		getMenuInflater().inflate(R.menu.activity_set_points, menu);
 		return true;
 	}
+	
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -58,10 +87,67 @@ public class SetPoints extends Activity {
 			//
 			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
 			//
-			NavUtils.navigateUpFromSameTask(this);
+		//	NavUtils.navigateUpFromSameTask(this);
+			finish();
 			return true;
 		}
-		return super.onOptionsItemSelected(item);
+		return false;//return super.onOptionsItemSelected(item);
 	}
+	
 
-}
+
+/* Gesture Dectector Class To Only listen On The Ones We Want */
+	
+	 public class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+		 //	private int x = 0;
+		 //	private int y = 0;
+	        private static final String DEBUG_TAG = "Gestures"; 	
+	/*Detect The Simple Gestures */
+	  @Override
+	    public boolean onSingleTapUp(MotionEvent event) {
+	        Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
+	        x = (int)event.getRawX();
+	        y = (int)event.getRawY();
+ 
+	       Log.d(DEBUG_TAG, "X is: " + x);
+	       Log.d(DEBUG_TAG, "Y is: " + y);
+	
+	        return true;
+	    }
+	  
+	  
+	   }
+	 
+	  public final int getX(){
+		  return x;  
+	  }
+	  
+	  public final int getY(){
+		  return y;  
+	  }
+	  public static  class GraphicView extends View{
+		  
+		  Paint dotColor = new Paint(Paint.ANTI_ALIAS_FLAG);
+		  
+		  
+	        public GraphicView(Context context){
+	            super(context);
+	        }
+
+	        @Override
+	        public void onDraw(Canvas canvas){
+	        	dotColor.setColor(0xff0000ff);
+	        	super.onDraw(canvas);
+	        	   Log.d("Graphic View","IM HERE");
+	        	if(getX() != 0 && getY() != 0){
+	        	//canvas.drawPoint(getX(), getY(), dotColor);
+	        	       canvas.drawCircle(getX(), getY(), 6, dotColor);
+	        	       Log.d("Graphic View","IM DRAWING");
+	        	   }
+	     
+	        	}
+	          
+	        }
+	  }
+
+
