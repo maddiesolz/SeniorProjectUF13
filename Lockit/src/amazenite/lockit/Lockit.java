defd24a16e9e6edc52463a238fb2fe7e7597f408
Kim Chan <kim.chan@ufl.edu>
2013-02-12 11:31:46 -0500

    Changed manifest for lock screen

--- Lockit/src/amazenite/lockit/Lockit.java
+++ Lockit/src/amazenite/lockit/Lockit.java
@@ -79,20 +79,15 @@ public class Lockit extends Activity {
 	
 	 public void saveImage2(Vector<Integer> images, int num)
 	    {
-	    	Log.d("save images", "im in it");
 	    	BitmapFactory.Options o = new BitmapFactory.Options();
-	    	Log.d("save images", "bit mpa factory open");
 		    o.inJustDecodeBounds = true;
-		    Log.d("save images", "w/e this thing is");
 	        final int size = 70;
 	        int scale = 2;
 	        while(o.outWidth/scale/2 >= size && o.outHeight/scale/2 >= size)
 	        {
 	        	scale *=2;
 	        }
-	        Log.d("save images", "did that");
      	BitmapFactory.Options o2 = new BitmapFactory.Options();
-     	Log.d("save images", "made it");
      	o2.inSampleSize=scale;
      	if(images.get(num) == null)
      	{
@@ -101,11 +96,9 @@ public class Lockit extends Activity {
      	Bitmap samplePic = BitmapFactory.decodeResource(getResources(), images.get(num), o2);
  		if(samplePic != null)
  		{
- 		Log.d("open Images", "Got the samplePic");
  		 try {
 	        	FileOutputStream fos = openFileOutput("lockimg", Context.MODE_PRIVATE);
 	        	samplePic.compress(CompressFormat.JPEG, 100, fos);
-	        	Log.d("open images", "compressed it");
 		        	try {
 		        		fos.close();
 		        		fos = null;
