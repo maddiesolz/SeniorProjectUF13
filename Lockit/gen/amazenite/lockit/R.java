c7316cba2cbad1710a6c2d819e64613327e260f5
Kim Chan <kim.chan@ufl.edu>
2013-03-19 12:34:24 -0400

    Voice Settings Layout

--- Lockit/gen/amazenite/lockit/R.java
+++ Lockit/gen/amazenite/lockit/R.java
@@ -25,43 +25,51 @@ public final class R {
         public static final int picturemenu=0x7f020005;
         public static final int pointset=0x7f020006;
         public static final int rainbow=0x7f020007;
-        public static final int voicemenu=0x7f020008;
+        public static final int recordvoice=0x7f020008;
+        public static final int voicemenu=0x7f020009;
     }
     public static final class id {
-        public static final int ChoosePicture=0x7f09000c;
-        public static final int ChoosePicturePreview=0x7f09000b;
-        public static final int PictureSettings=0x7f090009;
-        public static final int SpinnerSelect=0x7f090011;
+        public static final int ChoosePicture=0x7f09000b;
+        public static final int ChoosePicturePreview=0x7f09000a;
+        public static final int LinearLayout1=0x7f090015;
+        public static final int SpinnerSelect=0x7f090010;
         public static final int Tabs=0x7f090008;
-        public static final int VoiceSettings=0x7f09000a;
-        public static final int button1=0x7f090014;
-        public static final int button2=0x7f090015;
+        public static final int VoiceSettings=0x7f090009;
+        public static final int button1=0x7f090013;
+        public static final int button2=0x7f090014;
         public static final int cancel_button=0x7f090004;
         public static final int color_picker_view=0x7f090000;
         public static final int container=0x7f090006;
-        public static final int debugLockscreen=0x7f090010;
-        public static final int enablePicturePassword=0x7f09000f;
+        public static final int debugLockscreen=0x7f09000f;
+        public static final int enablePicturePassword=0x7f09000e;
+        public static final int imageButton1=0x7f090016;
         public static final int imageView1=0x7f090005;
-        public static final int iv_icon=0x7f090017;
-        public static final int menu_settings=0x7f090019;
+        public static final int iv_icon=0x7f09001c;
+        public static final int menu_settings=0x7f09001e;
         public static final int new_color_panel=0x7f090002;
         public static final int ok_button=0x7f090003;
         public static final int old_color_panel=0x7f090001;
-        public static final int preview=0x7f09000d;
-        public static final int setPoints=0x7f09000e;
-        public static final int setPointsPic=0x7f090016;
-        public static final int spinner=0x7f090013;
-        public static final int textView1=0x7f090012;
+        public static final int pictureSettings=0x7f090019;
+        public static final int preview=0x7f09000c;
+        public static final int recordVoice=0x7f09001b;
+        public static final int setPoints=0x7f09000d;
+        public static final int setPointsPic=0x7f090017;
+        public static final int spinner=0x7f090012;
+        public static final int tabs=0x7f090018;
+        public static final int textView1=0x7f090011;
         public static final int titleBar=0x7f090007;
-        public static final int tv_name=0x7f090018;
+        public static final int tv_name=0x7f09001d;
+        public static final int voiceSettings=0x7f09001a;
     }
     public static final class layout {
         public static final int activity_color_picker=0x7f030000;
         public static final int activity_color_picker_dialog=0x7f030001;
         public static final int activity_lock_screen=0x7f030002;
         public static final int activity_lockit=0x7f030003;
-        public static final int activity_set_points=0x7f030004;
-        public static final int crop_selecter=0x7f030005;
+        public static final int activity_record_voice=0x7f030004;
+        public static final int activity_set_points=0x7f030005;
+        public static final int activity_voice_settings=0x7f030006;
+        public static final int crop_selecter=0x7f030007;
     }
     public static final class menu {
         public static final int activity_color_picker=0x7f080000;
@@ -70,7 +78,9 @@ public final class R {
         public static final int activity_lock_screen=0x7f080003;
         public static final int activity_lockit=0x7f080004;
         public static final int activity_main=0x7f080005;
-        public static final int activity_set_points=0x7f080006;
+        public static final int activity_record_voice=0x7f080006;
+        public static final int activity_set_points=0x7f080007;
+        public static final int activity_voice_settings=0x7f080008;
     }
     public static final class string {
         /**  App Label On Top 
@@ -78,27 +88,27 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050006;
         public static final int menu_settings=0x7f050004;
-        public static final int pref_default_display_name=0x7f050010;
-        public static final int pref_description_social_recommendations=0x7f05000e;
+        public static final int pref_default_display_name=0x7f050012;
+        public static final int pref_description_social_recommendations=0x7f050010;
         /**  Example settings for Data & Sync 
          */
-        public static final int pref_header_data_sync=0x7f050012;
+        public static final int pref_header_data_sync=0x7f050014;
         /**  Strings related to Settings 
  Example General settings 
          */
-        public static final int pref_header_general=0x7f05000c;
+        public static final int pref_header_general=0x7f05000e;
         /**  Example settings for Notifications 
          */
-        public static final int pref_header_notifications=0x7f050015;
-        public static final int pref_ringtone_silent=0x7f050018;
-        public static final int pref_title_add_friends_to_messages=0x7f050011;
-        public static final int pref_title_display_name=0x7f05000f;
-        public static final int pref_title_new_message_notifications=0x7f050016;
-        public static final int pref_title_ringtone=0x7f050017;
-        public static final int pref_title_social_recommendations=0x7f05000d;
-        public static final int pref_title_sync_frequency=0x7f050013;
-        public static final int pref_title_system_sync_settings=0x7f050014;
-        public static final int pref_title_vibrate=0x7f050019;
+        public static final int pref_header_notifications=0x7f050017;
+        public static final int pref_ringtone_silent=0x7f05001a;
+        public static final int pref_title_add_friends_to_messages=0x7f050013;
+        public static final int pref_title_display_name=0x7f050011;
+        public static final int pref_title_new_message_notifications=0x7f050018;
+        public static final int pref_title_ringtone=0x7f050019;
+        public static final int pref_title_social_recommendations=0x7f05000f;
+        public static final int pref_title_sync_frequency=0x7f050015;
+        public static final int pref_title_system_sync_settings=0x7f050016;
+        public static final int pref_title_vibrate=0x7f05001b;
         public static final int setVisible=0x7f050005;
         public static final int title_activity_color_picker=0x7f050008;
         public static final int title_activity_color_picker_dialog=0x7f050009;
@@ -106,8 +116,10 @@ public final class R {
         public static final int title_activity_lock_screen=0x7f050003;
         public static final int title_activity_main=0x7f050007;
         public static final int title_activity_open_images=0x7f050001;
+        public static final int title_activity_record_voice=0x7f05000c;
         public static final int title_activity_set_points=0x7f050002;
-        public static final int title_activity_settings=0x7f05000b;
+        public static final int title_activity_settings=0x7f05000d;
+        public static final int title_activity_voice_settings=0x7f05000b;
     }
     public static final class style {
         /** 
---
+++ Lockit/src/amazenite/lockit/VoiceSettings.java
@@ -0,0 +1,29 @@
+package amazenite.lockit;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class VoiceSettings extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_voice_settings);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_voice_settings, menu);
+		return true;
+	}
+	
+	public void recordVoice()
+	{
+		Intent intent = new Intent(this, RecordVoice.class);
+		startActivity(intent);
+	}
+
+}
